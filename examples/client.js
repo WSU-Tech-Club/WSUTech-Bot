48506429193eee3fd317a55746f4aa01beafd8fc
Travis Person <travis.person@gmail.com>
2012-10-16 23:44:05 -0700

    Inital Commit

---
+++ examples/client.js
@@ -0,0 +1,10 @@
+PubSub = require('./lib/pubsub/pubsub') 
+PubSub.connect(5050, function (net) {
+
+    net.subscribe('event', 'message', function (topic, pipe, data) {
+        console.log(topic, pipe, data)
+    });
+
+
+    net.publish('client', 'say', {target: "#wsutech", message: "Hello! This is a test of the PubSub network."});
+});
---
+++ return_client.js
@@ -0,0 +1,31 @@
+PubSub = require('./bot') 
+
+PubSub.start(5050)
+
+net = PubSub.connect(5050);
+
+
+
+net.subscribe('foo', 'data', function (topic, pipe, data) {
+    console.log(topic, pipe, data)
+});
+
+net.subscribe('bar', 'data', function (topic, pipe, data) {
+    console.log(topic, pipe, data)
+});
+
+var foo = 0;
+var bar = 0;
+
+setInterval(function () {
+    net.publish('foo', 'data', foo++);
+}, 5000);
+
+setInterval(function () {
+    net.publish('bar', 'data', bar++);
+}, 7000);
+
+setInterval(function () {
+    console.log("Connected: ", net.connected);
+}, 1000);
+
