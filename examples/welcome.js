7316225613b8626e0af1439dbde4572408d15d40
Travis Person <travis.person@gmail.com>
2012-10-17 22:16:34 -0700

    Added comments

--- examples/welcome.js
+++ examples/welcome.js
@@ -1,14 +1,34 @@
 PubSub = require('../lib/pubsub/pubsub') 
 PubSub.connect(5050, function (net) {
 
+    //
+    //  Subscribe to the join event
+    //
+
     net.subscribe('event', 'join', function (topic, pipe, data) {
+    
+        // Log to console
+        console.log(topic, pipe, data)
+        
+        //
+        // Publish a message
+        //
+        
         net.publish('client', 'say', {target: data.channel, message: "Hello " + data.nick + "!"})
-        var channel = data.channel
+        
+        //
+        // Perform a whois lookup and accept the answer as a callback function
+        //
+
         net.publish('client', 'whois', {nick: data.nick}, function (data) {
             console.log("whois: ", data.info)
         });
-        console.log(topic, pipe, data)
+
     });
+
+    //
+    // Publish a loaded message
+    //
 
     net.publish('client', 'say', {target: "#wsutech", message: "! - [Plugin][Loaded] | Welcome Example"});
 });
